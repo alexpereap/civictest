26e61c4e7b3a069886152b50ce627fbfe8dbdc2a
Alex Perea <ing.alexperea@gmail.com>
2024-11-25 07:01:54 -0500

    moved headers to index

--- backend/src/App.php
+++ backend/src/App.php
@@ -2,10 +2,6 @@
 
 require_once dirname(__FILE__) . '/Router.php';
 
-// Allows access from any origin to allow frontend app to do request
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-
 /**
  * MAIN APP class
  */
